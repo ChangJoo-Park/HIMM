3dd5532b11510f9d2dd4870640bcfe27ffb030eb
Chang Ju Park <pcjpcj2@gmail.com>
2012-11-17 23:56:54 +0900

    데이터 베이스 추가
    DB추가

---
+++ 2012-11-04(박창주)-카메라.zip/com.wordpress.parkcj/assets/www/js/googlemap.js
@@ -0,0 +1,44 @@
+function GoogleMap(){
+    
+    this.initialize = function(){
+        var map = showMap();
+        addMarkersToMap(map);
+    };
+    
+    var addMarkersToMap = function(map){
+        var mapBounds = new google.maps.LatLngBounds();
+    
+        var latitudeAndLongitudeOne = new google.maps.LatLng('-33.890542','151.274856');
+
+        var markerOne = new google.maps.Marker({
+					position: latitudeAndLongitudeOne,
+					map: map
+				});
+				
+        var latitudeAndLongitudeTwo = new google.maps.LatLng('57.77828', '14.17200');
+
+        var markerOne = new google.maps.Marker({
+					position: latitudeAndLongitudeTwo,
+					map: map
+				});
+				
+        mapBounds.extend(latitudeAndLongitudeOne);
+        mapBounds.extend(latitudeAndLongitudeTwo);
+        
+        map.fitBounds(mapBounds);
+    };
+    
+    
+    
+    var showMap = function(){
+        var mapOptions = {
+			     zoom: 4,
+			     center: new google.maps.LatLng(-33, 151),
+			     mapTypeId: google.maps.MapTypeId.ROADMAP
+			 };
+			 
+        var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        
+        return map;
+    };
+}
